3a3f519f0008f19f020b31400c290ced625f63f5
BilalMaqsood7869 <bilalmaqsood7869@gmail.com>
2024-08-17 20:49:09 +0500

    17082024-3

---
+++ 3/3-calculator.py
@@ -0,0 +1,24 @@
+a = 20
+b = 10
+
+sum = a+b
+print("the sum is",sum)
+
+diff = a-b
+print("the diffrence is",diff)
+
+mul = a*b
+print("the multipal",mul)
+ 
+div = a/b
+print(" the division is",div)
+
+
+
+
+
+
+
+
+
+
