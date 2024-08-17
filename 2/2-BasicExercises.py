b1977b815a9ef2ca4a9385bd51fefcf97b9a4fe1
BilalMaqsood7869 <bilalmaqsood7869@gmail.com>
2024-08-17 20:33:17 +0500

    17082024

---
+++ 2/2-BasicExercises.py
@@ -0,0 +1,112 @@
+msg1 = "This is 1st message in variable saved as string."
+print(msg1)
+
+
+msg1 = "This is original message in variable saved as string."
+print(msg1)
+msg1 = "This is changed message in variable saved as string."
+print(msg1)
+
+
+name = "Ahmed Ali"
+print("Hello",name,", would you like to learn some Python today?")
+
+name2 = "Ahmed Abdullah"
+print(name2)
+low_name2 = name2.lower()
+print(low_name2)
+
+name3 = "Babar Azam"
+print(name3)
+upper_name3 = name3.upper()
+print(upper_name3)
+
+name4 = "Alber Einstein"
+quote = "A person who never made a mistake never tried anything new."
+print(name4, "once said,", quote)
+
+
+name5 = " Muhammad \tRashid \n Latif "
+print(name5)
+lstrip_name5 = name5.lstrip()
+print(lstrip_name5)
+
+a = 5
+b = 10
+c = a+b
+print(c)
+
+x = 10
+y = 20
+print("The value of x before swap is: ",x)
+print("The value of y before swap is: ",y)
+x, y = y, x
+print("The value of x after swap is: ",x)
+print("The value of y after swap is: ",y)
+
+color = "RED"
+print("My favoit color is:",color)
+color = "Blue"
+print("My favoit color is:",color)
+
+pet = "Buddy"
+print(pet)
+pet = "Max"
+print(pet)
+
+
+vr1 = "Sunshine"
+print(vr1)
+#print(vr2)
+
+score = 100
+print(score)
+score = 200
+print(score)
+
+temperature = 25
+print("The current temperature is" , temperature)
+
+
+poem = "Dil Dil Pakistan \nJaan Jaan Pakistan\n"
+print(poem)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
